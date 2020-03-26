cf1cc1e6046a575cc1748596f470f9a0be7e1a8e
Seong <pok_gare@naver.com>
2020-03-26 18:04:17 +0900

    feat(be): user service 추가
    회원가입, 회원가입 validation, 탈퇴, 회원찾기(by criteria)

--- server/src/main/java/daitnu/daitnus2/repository/UserRepository.java
+++ server/src/main/java/daitnu/daitnus2/repository/UserRepository.java
@@ -5,7 +5,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
-import javax.persistence.NoResultException;
 import java.util.List;
 
 @Repository
---
+++ server/src/main/java/daitnu/daitnus2/service/UserService.java
@@ -0,0 +1,60 @@
+package daitnu.daitnus2.service;
+
+import daitnu.daitnus2.domain.User;
+import daitnu.daitnus2.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    // 회원 가입
+    @Transactional
+    public Long register(User user) {
+        validateDuplicateUser(user);
+        userRepository.save(user);
+        return user.getId();
+    }
+
+    // 회원 가입 validation
+    private void validateDuplicateUser(User user) {
+        List<User> foundUser = userRepository.findOneByUserId(user.getUserId());
+        if (!foundUser.isEmpty()) {
+            throw new IllegalStateException("already registered user");
+        }
+    }
+
+    // 회원 탈퇴
+    public Long withDraw(User user) {
+        userRepository.delete(user);
+        return user.getId();
+    }
+
+    public User findOne(Long id) {
+        return userRepository.findOne(id);
+    }
+
+    public List<User> findAll() {
+        return userRepository.findAll();
+    }
+
+    public List<User> findOneByUserId(String userId) {
+        return userRepository.findOneByUserId(userId);
+    }
+
+    public List<User> findOneByUserEmail(String userEmail) {
+        return userRepository.findOneByUserEmail(userEmail);
+    }
+
+    // 비밀번호 찾기시 사용할 예정
+    public List<User> findOneByUserIdAndSubEmail(String userId, String subEmail) {
+        return userRepository.findOneByUserIdAndSubEmail(userId, subEmail);
+    }
+}
