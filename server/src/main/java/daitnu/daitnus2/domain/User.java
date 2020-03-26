8b3c22d490185c6e7664f40b266d5d08282e934f
Seong <pok_gare@naver.com>
2020-03-26 16:30:32 +0900

    feat(be): User 엔티티 추가
    비밀번호 처리는 추후에 Spring Security 공부하고 해야할 듯

---
+++ server/src/main/java/daitnu/daitnus2/domain/User.java
@@ -0,0 +1,41 @@
+package daitnu.daitnus2.domain;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.validation.constraints.NotBlank;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+
+    @Id @GeneratedValue
+    private Long id;
+
+    @NotBlank
+    private String userId;
+
+    @NotBlank
+    private String pw;
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String subEmail;
+
+    public User(String userId, String pw, String name, String email, String subEmail) {
+        this.userId = userId;
+        this.pw = pw;
+        this.name = name;
+        this.email = email;
+        this.subEmail = subEmail;
+    }
+}
