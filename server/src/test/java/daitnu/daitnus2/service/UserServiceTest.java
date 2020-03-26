2f90c026747d8a128611f3c4a75c509e26810198
Seong <pok_gare@naver.com>
2020-03-26 18:47:54 +0900

    test(be): 같은 아이디 가입 예외 추가

--- server/src/test/java/daitnu/daitnus2/service/UserServiceTest.java
+++ server/src/test/java/daitnu/daitnus2/service/UserServiceTest.java
@@ -32,4 +32,25 @@ public class UserServiceTest {
         // then
         assertEquals(user, userRepository.findOne(newUserId));
     }
+
+    @Test(expected = IllegalStateException.class)
+    public void 같은아이디_중복처리() throws Exception {
+        // given
+        User user1 = new User("kimsoso1", "1234", "kss1",
+                "kimsoso1@daitnu2.com", "kimsoso1@gaver.com");
+        User user2 = new User("kimsoso1", "1234", "kss2",
+                "kimsoso1@daitnu2.com", "kimsoso1@gaver.com");
+
+        // when
+        userService.register(user1);
+        userService.register(user2);
+
+        // then
+        fail("중복 아이디 가입은 예외가 발생해야 함");
+    }
+
+    @Test
+    public void 해당하는_아이디와_서브이메일을_가진_유저가_존재하지_않습니다() throws Exception {
+
+    }
 }
