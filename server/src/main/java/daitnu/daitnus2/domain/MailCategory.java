6aa08d97b776442ca0440fb70ee2b82f29c224bb
Seong <pok_gare@naver.com>
2020-03-27 18:02:59 +0900

    feat(be): 메일 카테로리 entity 생성자, update 추가

--- server/src/main/java/daitnu/daitnus2/domain/MailCategory.java
+++ server/src/main/java/daitnu/daitnus2/domain/MailCategory.java
@@ -1,10 +1,12 @@
 package daitnu.daitnus2.domain;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
 @Entity @Getter
+@NoArgsConstructor
 public class MailCategory {
 
     @Id @GeneratedValue
@@ -15,4 +17,13 @@ public class MailCategory {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "user_id")
     private User user;
+
+    public MailCategory(String name, User user) {
+        this.name = name;
+        this.user = user;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
 }
