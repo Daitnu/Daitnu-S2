de14d1e32d43d92143f526cf8dc4ff83ba41492a
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-03-28 18:28:13 +0900

    feat: AccountController
    어카운트 컨트롤러 실험용으로 만듬
    메서드는 바꿀예정

--- server/src/main/java/daitnu/daitnus2/DaitnuS2Application.java
+++ server/src/main/java/daitnu/daitnus2/DaitnuS2Application.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DaitnuS2Application {
-
-	public static void main(String[] args) {
-		SpringApplication.run(DaitnuS2Application.class, args);
-	}
-
+  public static void main(String[] args) {
+    SpringApplication.run(DaitnuS2Application.class, args);
+  }
 }
