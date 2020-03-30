9244c0feb53829d8ff540871ddd0b6af62cc2d06
Seong <pok_gare@naver.com>
2020-03-30 20:44:31 +0900

    chore(be): 테스트 디비 세팅 - daitnu_s2_test

--- server/src/test/java/daitnu/daitnus2/service/UserServiceTest.java
+++ server/src/test/java/daitnu/daitnus2/service/UserServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -16,6 +17,7 @@ import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 @Transactional
 public class UserServiceTest {
 
