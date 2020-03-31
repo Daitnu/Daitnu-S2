457607dbda6a892c894bf1cf892889b1c8845356
Seong <pok_gare@naver.com>
2020-03-31 15:51:18 +0900

    feat(be): mail receiver repository 추가
    save remove findOne findAll findAllByMailTemplateId

---
+++ server/src/main/java/daitnu/daitnus2/repository/MailReceiverRepository.java
@@ -0,0 +1,41 @@
+package daitnu.daitnus2.repository;
+
+import daitnu.daitnus2.domain.MailReceiver;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class MailReceiverRepository {
+
+  private final EntityManager em;
+
+  public void save(MailReceiver mailReceiver) {
+    if (mailReceiver.getId() == null) {
+      em.persist(mailReceiver);
+    } else {
+      em.merge(mailReceiver);
+    }
+  }
+
+  public void remove(MailReceiver mailReceiver) {
+    em.remove(mailReceiver);
+  }
+
+  public MailReceiver findOne(Long id) {
+    return em.find(MailReceiver.class, id);
+  }
+
+  public List<MailReceiver> findAll() {
+    return em.createQuery("select mr from MailReceiver mr", MailReceiver.class).getResultList();
+  }
+
+  public List<MailReceiver> findAllByMailTemplateId(Long mailTemplateId) {
+    return em.createQuery("select mr from MailReceiver mr where mr.mailTemplate = :mailTemplateId", MailReceiver.class)
+      .setParameter("mailTemplateId", mailTemplateId)
+      .getResultList();
+  }
+}
