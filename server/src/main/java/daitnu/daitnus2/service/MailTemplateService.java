80cbdda79088538af0c046fcc3c598910dcc14a8
Seong <pok_gare@naver.com>
2020-04-01 19:45:59 +0900

    feat(be): mail template service 추가
    make, remove, findOne, findAll

---
+++ server/src/main/java/daitnu/daitnus2/service/MailTemplateService.java
@@ -0,0 +1,41 @@
+package daitnu.daitnus2.service;
+
+import daitnu.daitnus2.domain.MailTemplate;
+import daitnu.daitnus2.repository.MailTemplateRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class MailTemplateService {
+
+  private final MailTemplateRepository mailTemplateRepository;
+
+  // 메일 템플릿 추가
+  @Transactional
+  public Long makeMailTemplate(MailTemplate mailTemplate) {
+    mailTemplateRepository.save(mailTemplate);
+    return mailTemplate.getId();
+  }
+
+  // 메일 템플릿 삭제
+  @Transactional
+  public Long removeMailTemplate(MailTemplate mailTemplate) {
+    mailTemplateRepository.remove(mailTemplate);
+    return mailTemplate.getId();
+  }
+
+  // find one
+  public MailTemplate findOne(Long id) {
+    return mailTemplateRepository.findOne(id);
+  }
+
+  // find all
+  public List<MailTemplate> findAll() {
+    return mailTemplateRepository.findAll();
+  }
+}
