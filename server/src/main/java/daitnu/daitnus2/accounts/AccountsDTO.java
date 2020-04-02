cdfa8e78c8478b751568a663b905286ce47d06e3
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-04-02 23:42:58 +0900

    feat(be/accounts): register dto작성

--- server/src/main/java/daitnu/daitnus2/accounts/AccountsDTO.java
+++ server/src/main/java/daitnu/daitnus2/accounts/AccountsDTO.java
@@ -4,6 +4,8 @@ import lombok.Getter;
 import lombok.Setter;
 import org.hibernate.validator.constraints.Length;
 
+import javax.persistence.Column;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 
 public class AccountsDTO {
@@ -11,7 +13,6 @@ public class AccountsDTO {
   @Getter
   @Setter
   public static class LoginDTO {
-
     @NotBlank(message = "아이디는 반드시 입력해야 합니다.")
     @Length(min = 5, message = "아이디는 최소 5자리 이상입니다.")
     @Length(max = 20, message = "아이디는 최대 20자리 입니다.")
@@ -21,5 +22,34 @@ public class AccountsDTO {
     @Length(min = 5, message = "비밀번호는 최소 5자리 이상입니다.")
     @Length(max = 100, message = "비밀번호는 최대 30자리 입니다.")
     private String password;
+  }
+
+  @Getter
+  @Setter
+  public static class RegisterDTO {
+    @NotBlank(message = "아이디는 반드시 입력해야 합니다.")
+    @Length(min = 5, message = "아이디는 최소 5자리 이상입니다.")
+    @Length(max = 20, message = "아이디는 최대 20자리 입니다.")
+    private String id;
+
+    @NotBlank(message = "비밀번호는 반드시 입력해야 합니다.")
+    @Length(min = 5, message = "비밀번호는 최소 5자리 이상입니다.")
+    @Length(max = 100, message = "비밀번호는 최대 30자리 입니다.")
+    private String password;
+
+    @NotBlank(message = "비밀번호 확인은 반드시 입력해야 합니다.")
+    @Length(min = 5, message = "비밀번호 확인은 최소 5자리 이상입니다.")
+    @Length(max = 100, message = "비밀번호 확인은 최대 30자리 입니다.")
+    private String passwordCheck;
+
+    @NotBlank(message = "이름은 반드시 입력해야 합니다.")
+    @Length(min = 2, message = "이름은 최소 2자리 이상입니다.")
+    @Length(max = 10, message = "이름은 최대 길이 10입니다.")
+    private String name;
+
+    @NotBlank
+    @Email(message = "서브 이메일은 이메일 주소여야 합니다.")
+    @Length(max = 50, message = "서브 이메일의 최대 길이는 50입니다.")
+    private String subEmail;
   }
 }
