b738673ac84b3cc04b332df1c6566ab1a4486822
Seong <pok_gare@naver.com>
2020-04-03 20:03:11 +0900

    test(be): 메일 생성 통합 테스트 추가
    세부 테스트 추가 예정..

---
+++ server/src/test/java/daitnu/daitnus2/service/MailRelatedIntegrationTest.java
@@ -0,0 +1,106 @@
+package daitnu.daitnus2.service;
+
+import daitnu.daitnus2.mail.Mail;
+import daitnu.daitnus2.mail.MailService;
+import daitnu.daitnus2.mail.attachment.MailAttachment;
+import daitnu.daitnus2.mail.attachment.MailAttachmentService;
+import daitnu.daitnus2.mail.category.MailCategory;
+import daitnu.daitnus2.mail.category.MailCategoryService;
+import daitnu.daitnus2.mail.template.MailTemplate;
+import daitnu.daitnus2.mail.template.MailTemplateService;
+import daitnu.daitnus2.mail.template.receiver.MailReceiver;
+import daitnu.daitnus2.mail.template.receiver.MailReceiverService;
+import daitnu.daitnus2.user.User;
+import daitnu.daitnus2.user.UserService;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@Transactional
+public class MailRelatedIntegrationTest {
+
+  @Autowired UserService userService;
+  @Autowired MailService mailService;
+  @Autowired MailCategoryService mailCategoryService;
+  @Autowired MailTemplateService mailTemplateService;
+  @Autowired MailReceiverService mailReceiverService;
+  @Autowired MailAttachmentService mailAttachmentService;
+
+  @Test
+  public void 메일_생성_통합테스트() {
+    // given
+    User user = new User("kimsoso", "1234", "kss", "kimsoso@gaver.com");
+    MailCategory category = new MailCategory("category1", user);
+    MailTemplate mailTemplate = new MailTemplate("kimgogo@daitnu2.com", "mail title1", "mail subject1");
+    MailReceiver mailReceiver = new MailReceiver(mailTemplate, "kimsoso@daitnu2.com");
+    MailAttachment mailAttachment =
+      new MailAttachment(mailTemplate, "png", "image1.png", "https://www.naver.com", 10L);
+    Mail mail = new Mail(category, user, mailTemplate);
+
+
+    // when
+    user.addMailCategory(category);
+    mailTemplate.addMail(mail);
+    mailTemplate.addReceiver(mailReceiver);
+    mailTemplate.addAttachment(mailAttachment);
+
+    userService.register(user);
+    mailTemplateService.makeMailTemplate(mailTemplate);
+    mailService.makeMail(mail);
+
+
+    // then
+    User foundUser = userService.findOne(user.getId());
+    MailCategory foundCategory = mailCategoryService.findOne(category.getId());
+    MailReceiver foundReceiver = mailReceiverService.findOne(mailReceiver.getId());
+    MailAttachment foundAttachment = mailAttachmentService.findOne(mailAttachment.getId());
+    MailTemplate foundTemplate = mailTemplateService.findOne(mailTemplate.getId());
+    Mail foundMail = mailService.findOne(mail.getId());
+
+    // - User Assert
+    assertEquals(user.getId(), foundUser.getId());
+    assertEquals(1, userService.findAll().size());
+    assertEquals(1, user.getMailCategories().size());
+
+    // - Category Assert
+    assertEquals(category.getId(), foundCategory.getId());
+    assertEquals(1, mailCategoryService.findAll().size());
+    assertEquals(foundUser.getId(), foundCategory.getUser().getId());
+    assertEquals(foundCategory.getId(), foundUser.getMailCategories().get(0).getId());
+
+    // - Mail Receiver
+    assertEquals(mailReceiver.getId(), foundReceiver.getId());
+    assertEquals(1, mailReceiverService.findAll().size());
+    assertEquals(foundTemplate.getId(), foundReceiver.getMailTemplate().getId());
+    assertEquals(foundReceiver.getId(), foundTemplate.getMailReceivers().get(0).getId());
+
+    // - Mail Attachment
+    assertEquals(mailAttachment.getId(), foundAttachment.getId());
+    assertEquals(1, foundAttachment.getMailTemplate().getMailReceivers().size());
+    assertEquals(foundAttachment.getId(), foundTemplate.getMailAttachments().get(0).getId());
+
+    // - Mail Template
+    assertEquals(mailTemplate.getId(), foundTemplate.getId());
+    assertEquals(1, mailTemplateService.findAll().size());
+    assertEquals(1, foundTemplate.getMailReceivers().size());
+    assertEquals(1, foundTemplate.getMailAttachments().size());
+
+    // - Mail
+    assertEquals(mail.getId(), foundMail.getId());
+    assertEquals(1, mailService.findAll().size());
+    assertEquals(foundUser.getId(), foundMail.getOwner().getId());
+    assertEquals(foundCategory.getId(), foundMail.getCategory().getId());
+    assertEquals(foundTemplate.getId(), foundMail.getMailTemplate().getId());
+  }
+}
