06c9c2e2d033b394f79a8f801936dbd2758bcbdb
Seong <pok_gare@naver.com>
2020-04-03 20:03:44 +0900

    chore(be): 안쓰는 import 삭제

--- server/src/test/java/daitnu/daitnus2/service/MailRelatedIntegrationTest.java
+++ server/src/test/java/daitnu/daitnus2/service/MailRelatedIntegrationTest.java
@@ -20,8 +20,6 @@ import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 import static org.junit.Assert.*;
 
 @RunWith(SpringRunner.class)
