2107a894250f26999672e2b3fafa6fd66bb8d9b0
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-04-04 23:46:41 +0900

    feat(be/repo): user Repo findByUserIdOrSubEmail
    중복검사를 위한 쿼리

--- server/src/main/java/daitnu/daitnus2/database/repository/UserRepository.java
+++ server/src/main/java/daitnu/daitnus2/database/repository/UserRepository.java
@@ -6,4 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 public interface UserRepository extends JpaRepository<User,Long> {
 
   User findByUserId(String userId);
+  User findByUserIdOrSubEmail(String userId,String subEmail);
 }
