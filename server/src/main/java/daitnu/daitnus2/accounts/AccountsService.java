37e9d782cf995494a47b1f76b09b89497a06d02f
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-04-04 23:49:19 +0900

    feat(be/accounts): check duplicate when register

--- server/src/main/java/daitnu/daitnus2/accounts/AccountsService.java
+++ server/src/main/java/daitnu/daitnus2/accounts/AccountsService.java
@@ -1,14 +1,18 @@
 package daitnu.daitnus2.accounts;
 
+import daitnu.daitnus2.accounts.exception.DuplicatUserId;
+import daitnu.daitnus2.accounts.exception.DuplicateSubEmail;
 import daitnu.daitnus2.accounts.exception.InvalidLoginInput;
 import daitnu.daitnus2.database.entity.User;
 import daitnu.daitnus2.database.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
+@Transactional
 public class AccountsService {
 
   private final UserRepository userRepository;
@@ -21,13 +25,13 @@ public class AccountsService {
     String userPw = dto.getPassword();
 
     User user = userRepository.findByUserId(userId);
-    if(user == null) {
+    if (user == null) {
       throw new InvalidLoginInput();
     }
 
     String encodedPw = user.getPw();
-    boolean isValidPw = passwordEncoder.matches(userPw,encodedPw);
-    if(isValidPw == false) {
+    boolean isValidPw = passwordEncoder.matches(userPw, encodedPw);
+    if (isValidPw == false) {
       throw new InvalidLoginInput();
     }
 
@@ -39,10 +43,17 @@ public class AccountsService {
     String userPw = dto.getPassword();
     String userSubEmail = dto.getSubEmail();
     String userName = dto.getName();
-    User newUser = new User(userId,userPw,userSubEmail,userName);
 
-    // TODO : SubEmail unique 확인, Id duplicate 확인
-    User createdUser = userRepository.save(newUser);
-    return createdUser;
+
+    User searchedUser = userRepository.findByUserIdOrSubEmail(userId, userSubEmail);
+    if (searchedUser != null) {
+      String searchedUserId = searchedUser.getUserId();
+      boolean isDuplicateUserId = searchedUserId.equals(userId);
+      throw isDuplicateUserId ? new DuplicatUserId() : new DuplicateSubEmail();
+    }
+
+    User userInstance = new User(userId, userPw, userName, userSubEmail);
+    User newUser = userRepository.save(userInstance);
+    return newUser;
   }
 }
