4b1c4fd7e843eb2f7d3a7f7d4a9ee4b86ffca04b
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-04-04 23:14:28 +0900

    chore(be/bean): modelmapper bean등록

--- server/src/main/java/daitnu/daitnus2/DaitnuS2Application.java
+++ server/src/main/java/daitnu/daitnus2/DaitnuS2Application.java
@@ -1,11 +1,19 @@
 package daitnu.daitnus2;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class DaitnuS2Application {
   public static void main(String[] args) {
     SpringApplication.run(DaitnuS2Application.class, args);
+  }
+
+  @Bean
+  public ModelMapper modelMapper() {
+    ModelMapper modelMapper = new ModelMapper();
+    return modelMapper;
   }
 }
