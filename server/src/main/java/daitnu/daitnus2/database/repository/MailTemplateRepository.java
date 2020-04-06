f7aad0013ea61e3b453fb9a88f3e1d4447529b57
Seong <pok_gare@naver.com>
2020-04-06 19:18:22 +0900

    feat(be): JpaRepository interface를 활용한 mail template repository 추가

---
+++ server/src/main/java/daitnu/daitnus2/database/repository/MailTemplateRepository.java
@@ -0,0 +1,8 @@
+package daitnu.daitnus2.database.repository;
+
+import daitnu.daitnus2.database.entity.MailTemplate;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface MailTemplateRepository extends JpaRepository<MailTemplate, Long> {
+
+}
