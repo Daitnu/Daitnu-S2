dd0601b4370573540e00b1d2f6bc726bb41957be
Seong <pok_gare@naver.com>
2020-04-07 22:16:49 +0900

    fix(be): 테스트 코드 에러 수정
    통합테스트는 SpringBootTest, AutoConfigureMockMvc 애노테이션을 함께 붙여야 함
    body내용은 content로

--- server/src/test/java/daitnu/daitnus2/service/MailCategoryControllerTest.java
+++ server/src/test/java/daitnu/daitnus2/service/MailCategoryControllerTest.java
@@ -7,17 +7,23 @@ import daitnu.daitnus2.user.UserService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.*;
 
-@WebMvcTest
+@SpringBootTest
+@AutoConfigureMockMvc
 @RunWith(SpringRunner.class)
 @ActiveProfiles("test")
 @Transactional
@@ -30,18 +36,23 @@ public class MailCategoryControllerTest {
   public void postControllerTest() throws Exception {
     // given
     MockHttpSession mockHttpSession = new MockHttpSession();
-    String mailboxName = "mailbox1";
     User user = new User("kimsoso", "1234", "kss", "kimsoso@gaver.com");
-    MailCategory mailCategory = new MailCategory(mailboxName, user);
 
     // when
     userService.register(user);
-    mailCategoryService.makeDir(mailCategory);
     mockHttpSession.setAttribute("user", user);
 
     // then
-    mockMvc.perform(post("/mail/category").session(mockHttpSession))
+    mockMvc.perform(post("/mail/category")
+      .session(mockHttpSession)
+      .content("{\"mailCategoryName\": \"메일함이름1\"}")
+      .contentType(MediaType.APPLICATION_JSON_VALUE)
+      .accept(MediaType.APPLICATION_JSON_VALUE))
       .andExpect(status().isCreated())
     ;
+
+    List<MailCategory> categories = mailCategoryService.findAll("kimsoso");
+    assertEquals(1, categories.size());
+    assertEquals("메일함이름1", categories.get(0).getName());
   }
 }
