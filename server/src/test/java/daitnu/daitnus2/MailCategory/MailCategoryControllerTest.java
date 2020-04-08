22f6a0640a39fdca201d639ea581c6ec625d898d
Seong <pok_gare@naver.com>
2020-04-08 20:23:53 +0900

    feat(be/test): 메일함 추가 실패하는 테스트 추가

--- server/src/test/java/daitnu/daitnus2/MailCategory/MailCategoryControllerTest.java
+++ server/src/test/java/daitnu/daitnus2/MailCategory/MailCategoryControllerTest.java
@@ -16,12 +16,14 @@ import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.Assert.*;
 
@@ -65,5 +67,32 @@ public class MailCategoryControllerTest {
     List<MailCategory> categories = mailCategoryService.findAll("kimsoso");
     assertEquals(1, categories.size());
     assertEquals("메일함이름1", categories.get(0).getName());
+  }
+
+  @Test
+  public void 메일함_추가_실패_테스트1() throws Exception{
+    // given
+    User user = new User(userId, pw, name, subEmail);
+    MockHttpSession mockHttpSession = new MockHttpSession();
+    MailCategoryDTO.MakeDTO newCategory = new MailCategoryDTO.MakeDTO();
+
+    // when
+    newCategory.setMailCategoryName("ㅁㄴㅇㄹ");
+    userService.register(user);
+    mockHttpSession.setAttribute("user", user);
+    ResultActions result = mockMvc.perform(post("/mail/category")
+      .session(mockHttpSession)
+      .content(objectMapper.writeValueAsString(newCategory))
+      .contentType(MediaType.APPLICATION_JSON_VALUE)
+      .accept(MediaType.APPLICATION_JSON_VALUE))
+      .andDo(print())
+      .andExpect(status().isBadRequest());
+
+    // then
+    result
+      .andExpect(jsonPath("message").value("Invalid Input Value"))
+      .andExpect(jsonPath("status").value(400))
+      .andExpect(jsonPath("errors[0].field").value("mailCategoryName"))
+    ;
   }
 }
