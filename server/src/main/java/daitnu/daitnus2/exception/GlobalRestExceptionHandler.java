d219126da2600f134aa1afeecc2aa6ba19f2cf68
Seong <pok_gare@naver.com>
2020-04-08 20:24:50 +0900

    feat(be/test): method argument not valid exception handler 추가

--- server/src/main/java/daitnu/daitnus2/exception/GlobalRestExceptionHandler.java
+++ server/src/main/java/daitnu/daitnus2/exception/GlobalRestExceptionHandler.java
@@ -2,6 +2,7 @@ package daitnu.daitnus2.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -17,5 +18,10 @@ public class GlobalRestExceptionHandler {
   @ExceptionHandler(BusinessException.class)
   public ResponseEntity<?> handleUserNotExistException(BusinessException e) {
     return new ResponseEntity<>(ErrorResponse.of(e), HttpStatus.valueOf(e.getErrorCode().getStatus()));
+  }
+
+  @ExceptionHandler(MethodArgumentNotValidException.class)
+  public ResponseEntity<?> handle(MethodArgumentNotValidException e) {
+    return new ResponseEntity<>(ErrorResponse.of(ErrorCode.INVALID_INPUT_VALUE, e.getBindingResult()), HttpStatus.BAD_REQUEST);
   }
 }
