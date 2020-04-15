97c7735765445953105828b027225447e041380b
sjh2428 <pok_gare@naver.com>
2020-04-15 17:02:00 +0900

    feat(fe/login): on change handler 함수 작성

--- web/src/components/Forms/LoginForm/LoginForm.tsx
+++ web/src/components/Forms/LoginForm/LoginForm.tsx
@@ -1,7 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 import S from './styled';
 
+interface ILoginState {
+  userId: string;
+  password: string;
+}
+
+const loginInitState: ILoginState = {
+  userId: '',
+  password: '',
+};
+
 export const LoginForm: React.FC = () => {
+  const [loginState, setLoginState] = useState<ILoginState>(loginInitState);
+
+  const handleOnChange = (key: string) => ({ target: { value } }): void =>
+    setLoginState({ ...loginState, [key]: value }); // noImplicitAny옵션을 설정 안하면 value에서 빨간줄
+
   return (
     <S.InputForm>
       <S.Input
@@ -12,6 +27,8 @@ export const LoginForm: React.FC = () => {
         name="userId"
         maxLength={20}
         autoComplete="off"
+        value={loginState.userId}
+        onChange={handleOnChange('userId')}
       />
       <S.ErrorText></S.ErrorText>
       <S.Input
@@ -22,6 +39,8 @@ export const LoginForm: React.FC = () => {
         name="password"
         maxLength={20}
         autoComplete="off"
+        value={loginState.password}
+        onChange={handleOnChange('password')}
       />
       <S.ErrorText></S.ErrorText>
       <S.Button className="submit-btn max-width">로그인</S.Button>
