a416b6e7aaa0dc408866c6c6e009917dc8bf0a69
sjh2428 <pok_gare@naver.com>
2020-04-16 19:28:15 +0900

    feat(be/cors): cors configuration 추가

---
+++ server/src/main/java/daitnu/daitnus2/WebConfig.java
@@ -0,0 +1,14 @@
+package daitnu.daitnus2;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+  @Override
+  public void addCorsMappings(CorsRegistry registry) {
+    registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+  }
+}
