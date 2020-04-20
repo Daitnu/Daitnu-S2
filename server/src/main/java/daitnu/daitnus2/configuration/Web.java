21c458a70e16ecf430347efde01f574e7fcaa086
sjh2428 <pok_gare@naver.com>
2020-04-20 16:07:17 +0900

    chore(be/configuration): web configuration 파일 이동

--- server/src/main/java/daitnu/daitnus2/configuration/Web.java
+++ server/src/main/java/daitnu/daitnus2/configuration/Web.java
@@ -1,11 +1,11 @@
-package daitnu.daitnus2;
+package daitnu.daitnus2.configuration;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class WebConfig implements WebMvcConfigurer {
+public class Web implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
