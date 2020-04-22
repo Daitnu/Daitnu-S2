afb9c6aefc59dab5058363ea79e37e93b5e04b3f
Seong <pok_gare@naver.com>
2020-04-22 22:25:18 +0900

    feat(be/exception): patch 메소드로 request를 보낼 때 type error 처리를 위한 error code 추가

--- server/src/main/java/daitnu/daitnus2/exception/ErrorCode.java
+++ server/src/main/java/daitnu/daitnus2/exception/ErrorCode.java
@@ -9,6 +9,7 @@ public enum ErrorCode {
 
   // Common
   INVALID_INPUT_VALUE(400, "COMMON001", "Invalid Input Value"),
+  INVALID_TYPE_VALUE(400, "COMMON002", "Invalid Type Value"),
   HANDLE_ACCESS_DENIED(403, "COMMON003", "Access is Denied"),
   NOT_FOUND_EXCEPTION(404, "COMMON004", "Page Not Found"),
   METHOD_NOT_ALLOWED(405, "COMMON005", " Method Not Allowed"),
