88f05cfd7e408cc50b950d497c6b1c48d7d8b9d5
Seong <pok_gare@naver.com>
2020-04-22 22:46:05 +0900

    feat(be/test/mail): enum validation 체크겸 메일_정보_수정_실패_테스트 추가
    근데 왜 Invalid Type Value로 안뜨고 Invalid Input Value가 뜨누..

---
+++ server/src/test/java/daitnu/daitnus2/mail/MailControllerTest.java
@@ -0,0 +1,52 @@
+package daitnu.daitnus2.mail;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+import org.springframework.transaction.annotation.Transactional;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.junit.Assert.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@RunWith(SpringRunner.class)
+@ActiveProfiles("test")
+@Transactional
+public class MailControllerTest {
+  @Autowired MockMvc mockMvc;
+  @Autowired ObjectMapper objectMapper;
+
+  @Test
+  public void 메일_정보_수정_실패_테스트() throws Exception { // TODO: 왜 Invalid Input Value로 message가 뜨는가?
+    // given
+    MailDTO.MoveMailDTO dto = new MailDTO.MoveMailDTO();
+    dto.setType("LOTTOT");
+
+    // when
+    ResultActions result = mockMvc.perform(patch("/mail")
+      .content(objectMapper.writeValueAsString((dto)))
+      .contentType(MediaType.APPLICATION_JSON_VALUE)
+      .accept(MediaType.APPLICATION_JSON_VALUE))
+      .andDo(print());
+
+    // then
+    result
+      .andExpect(status().isBadRequest())
+      .andExpect(jsonPath("message").value("Invalid Type Value"))
+      .andExpect(jsonPath("status").value(400))
+      .andExpect(jsonPath("errors[0].field").value("type"))
+    ;
+  }
+}
