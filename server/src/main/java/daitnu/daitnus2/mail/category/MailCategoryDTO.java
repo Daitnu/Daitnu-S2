6d59691b8f3f0cedee6753c1c8d52e0c5e931c63
Seong <pok_gare@naver.com>
2020-04-23 17:16:29 +0900

    feat(be/mail/category): 메일함 삭제를 위한 dto 추가

--- server/src/main/java/daitnu/daitnus2/mail/category/MailCategoryDTO.java
+++ server/src/main/java/daitnu/daitnus2/mail/category/MailCategoryDTO.java
@@ -12,22 +12,34 @@ public class MailCategoryDTO {
   @Getter @Setter
   public static class MakeDTO {
     @NotBlank
-    @Pattern(regexp = "^[0-9a-zA-Z가-힣 ]{1,20}$", message = "메일함 이름은 1자이상 20자 이하, 완성된 한글, 영문, 숫자의 조합만 가능합니다")
+    @Pattern(regexp = "^[0-9a-zA-Z가-힣 ]{1,20}$",
+            message = "메일함 이름은 1자이상 20자 이하, 완성된 한글, 영문, 숫자의 조합만 가능합니다")
     private String name;
   }
 
   @Getter @Setter
   public static class RenameDTO {
     @NotBlank
-    @Pattern(regexp = "^[0-9a-zA-Z가-힣 ]{1,20}$", message = "메일함 이름은 1자이상 20자 이하, 완성된 한글, 영문, 숫자의 조합만 가능합니다")
+    @Pattern(regexp = "^[0-9a-zA-Z가-힣 ]{1,20}$",
+            message = "메일함 이름은 1자이상 20자 이하, 완성된 한글, 영문, 숫자의 조합만 가능합니다")
     private String oldName;
 
     @NotBlank
-    @Pattern(regexp = "^[0-9a-zA-Z가-힣 ]{1,20}$", message = "메일함 이름은 1자이상 20자 이하, 완성된 한글, 영문, 숫자의 조합만 가능합니다")
+    @Pattern(regexp = "^[0-9a-zA-Z가-힣 ]{1,20}$",
+            message = "메일함 이름은 1자이상 20자 이하, 완성된 한글, 영문, 숫자의 조합만 가능합니다")
     private String newName;
 
     @NotNull
     private Long categoryId;
+  }
+
+  @Getter @Setter
+  public static class DeleteDTO {
+    @NotNull
+    private Long id;
+
+    @NotBlank
+    private String name;
   }
 
   @Getter @Setter
