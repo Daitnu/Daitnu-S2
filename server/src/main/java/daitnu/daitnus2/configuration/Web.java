a75e6d2366ab6cf38c4f240f441528d61443a2a7
JongHwa <31912670+jonghwajoa@users.noreply.github.com>
2020-05-02 23:08:54 +0900

    feat(be/AuthInterceptor): 로그인여부 확인 인터셉터

--- server/src/main/java/daitnu/daitnus2/configuration/Web.java
+++ server/src/main/java/daitnu/daitnus2/configuration/Web.java
@@ -1,7 +1,9 @@
 package daitnu.daitnus2.configuration;
 
+import daitnu.daitnus2.interceptor.AuthInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -9,6 +11,11 @@ public class Web implements WebMvcConfigurer {
 
   @Override
   public void addCorsMappings(CorsRegistry registry) {
-    registry.addMapping("/**").allowedOrigins("http://localhost:3000");
+    registry.addMapping("/**").allowedOrigins("http://localhost:3000").allowedOrigins("*");
+  }
+
+  @Override
+  public void addInterceptors(InterceptorRegistry registry) {
+    registry.addInterceptor(new AuthInterceptor()).addPathPatterns("/mail/**");
   }
 }
---
+++ server/src/main/java/daitnu/daitnus2/interceptor/AuthInterceptor.java
@@ -0,0 +1,28 @@
+package daitnu.daitnus2.interceptor;
+
+import daitnu.daitnus2.accounts.AccountsSession;
+import daitnu.daitnus2.exception.BusinessException;
+import daitnu.daitnus2.exception.ErrorCode;
+import org.springframework.web.bind.annotation.SessionAttribute;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+public class AuthInterceptor implements HandlerInterceptor {
+
+  @Override
+  public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+    throws Exception {
+
+    HttpSession session = request.getSession();
+    AccountsSession sessionUser = (AccountsSession) session.getAttribute("user");
+
+    if (sessionUser == null) {
+      throw new BusinessException(ErrorCode.UNAUTHORIZED);
+    }
+
+    return true;
+  }
+}
