c35f8353f3d5835b04191a9bac400ac7688c0e85
sjh2428 <pok_gare@naver.com>
2020-05-04 20:05:35 +0900

    feat(fe): redux saga 미들웨어 추가

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -5,9 +5,17 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
-import rootReducer from './redux';
+import rootReducer, { rootSaga } from './redux';
+import createSagaMiddleware from 'redux-saga';
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(logger)));
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(sagaMiddleware, logger)),
+);
+
+sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
