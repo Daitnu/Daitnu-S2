4d9d9408d26253457fa60148e00a4b3be7af2f39
sjh2428 <pok_gare@naver.com>
2020-05-04 16:47:11 +0900

    feat(fe/api): register api 추가

--- web/src/library/request/UserApi.ts
+++ web/src/library/request/UserApi.ts
@@ -1,7 +1,20 @@
 import Api from './Api';
+import { ResponseRegister } from '~/@types/response/user';
+import { HTTPResponse } from '~/@types/response/success';
+import { BusinessErrorResponse } from '~/@types/response/error';
+import { RequestParam } from '~/@types/request/common';
+import { RegisterParam } from '~/@types/request/user';
+import URL from './url';
 
 export class UserApi extends Api {
   constructor() {
     super();
+  }
+
+  public async register({
+    url = URL.USER.REGISTER,
+    data,
+  }: RequestParam<RegisterParam>): Promise<HTTPResponse<ResponseRegister> | BusinessErrorResponse> {
+    return this.post<ResponseRegister, RegisterParam>({ url, data });
   }
 }
---
+++ web/src/library/request/url.ts
@@ -0,0 +1,6 @@
+export default {
+  USER: {
+    REGISTER: '/register',
+    LOGIN: '/login',
+  },
+};
