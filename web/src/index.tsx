b38debcf8bfa688803fbfe018befdbd70196dca7
Seong <pok_gare@naver.com>
2020-05-06 19:26:11 +0900

    feat(fe/style): global body style 수정
    margin 0, padding 0, font style

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -7,6 +7,16 @@ import { composeWithDevTools } from 'redux-devtools-extension';
 import logger from 'redux-logger';
 import rootReducer, { rootSaga } from './redux';
 import createSagaMiddleware from 'redux-saga';
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 0;
+    margin: 0;
+    font-family: "Malgun Gothic", "맑은 고딕", -apple-system, BlinkMacSystemFont, helvetica, 
+      "Apple SD Gothic Neo", sans-serif;
+  }
+`;
 
 const sagaMiddleware = createSagaMiddleware();
 
@@ -19,6 +29,7 @@ sagaMiddleware.run(rootSaga);
 
 ReactDOM.render(
   <Provider store={store}>
+    <GlobalStyle />
     <AppRouter />
   </Provider>,
   document.getElementById('root'),
