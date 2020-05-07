8c5e9f85269d11128e1ae3dbcc0afe3b21bfe98f
Seong <pok_gare@naver.com>
2020-05-07 18:56:47 +0900

    feat(fe/register): form state/change 이벤트 핸들러 추가

--- web/src/components/Forms/RegisterForm/RegisterForm.tsx
+++ web/src/components/Forms/RegisterForm/RegisterForm.tsx
@@ -1,20 +1,47 @@
-import React from 'react';
+import React, { useState, ChangeEvent } from 'react';
 import S from './styled';
+import { RegisterParam } from '~/@types/request/user';
+
+const initialState: RegisterParam = {
+  id: '',
+  password: '',
+  passwordCheck: '',
+  name: '',
+  subEmail: '',
+};
 
 export const RegisterForm: React.FC = () => {
+  const [formState, setFormState] = useState<RegisterParam>(initialState);
+  const handleInputChange = (key: string) => ({
+    target: { value },
+  }: ChangeEvent<HTMLInputElement>) => {
+    setFormState({ ...formState, [key]: value });
+  };
+
   return (
     <S.InputForm autoComplete="off">
       <S.InputContainer>
         <S.Title>Daitnu 계정 만들기</S.Title>
       </S.InputContainer>
       <S.InputContainer>
-        <S.Input id="name" placeholder="이름" />
+        <S.Input
+          id="name"
+          placeholder="이름"
+          value={formState.name}
+          onChange={handleInputChange('name')}
+        />
       </S.InputContainer>
       <S.InputContainer>
         <S.ErrorText>{/* errors.name */}</S.ErrorText>
       </S.InputContainer>
       <S.InputContainer>
-        <S.Input id="id" placeholder="아이디" maxLength={20} />
+        <S.Input
+          id="id"
+          placeholder="아이디"
+          maxLength={20}
+          value={formState.id}
+          onChange={handleInputChange('id')}
+        />
         <S.InputEndText>@daitnu2.com</S.InputEndText>
       </S.InputContainer>
       <S.InputContainer>
@@ -22,8 +49,20 @@ export const RegisterForm: React.FC = () => {
       </S.InputContainer>
       <S.InputContainer>
         <S.PasswordContainer>
-          <S.Input id="password" type="password" placeholder="비밀번호" />
-          <S.Input id="passwordCheck" type="password" placeholder="확인" />
+          <S.Input
+            id="password"
+            type="password"
+            placeholder="비밀번호"
+            value={formState.password}
+            onChange={handleInputChange('password')}
+          />
+          <S.Input
+            id="passwordCheck"
+            type="password"
+            placeholder="확인"
+            value={formState.passwordCheck}
+            onChange={handleInputChange('passwordCheck')}
+          />
           <S.PasswordVisibleBtn visible={false} />
         </S.PasswordContainer>
       </S.InputContainer>
@@ -31,7 +70,12 @@ export const RegisterForm: React.FC = () => {
         <S.ErrorText>{/* errors.password || errors.checkPassword */}</S.ErrorText>
       </S.InputContainer>
       <S.InputContainer>
-        <S.Input id="email" placeholder="이메일" />
+        <S.Input
+          id="email"
+          placeholder="이메일"
+          value={formState.subEmail}
+          onChange={handleInputChange('email')}
+        />
       </S.InputContainer>
       <S.InputContainer>
         <S.ErrorText>{/* errors.register || errors.email */}</S.ErrorText>
