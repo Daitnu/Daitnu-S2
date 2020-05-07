1e41f7a78735489e7fd573ed95797862270ce951
Seong <pok_gare@naver.com>
2020-05-07 19:37:08 +0900

    fix(fe/linkarea): 해당 path로 이동하기 위한 a태그 수정
    마치 서버사이드 렌더링에서 이동하는것처럼 이동해서
    뭔가 봤더니 a태그로 이동중이었음
    Link태그를 이용했을 시 깜빡임 없이
    부드럽게 이동(만족 ㅎ)

--- web/src/components/LinkArea/LinkArea.tsx
+++ web/src/components/LinkArea/LinkArea.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import * as S from './styled';
 
-export const LinkArea: React.FC = () => (
-  <S.Footer>
-    <a href="help/id">
-      <S.TextLink>아이디 찾기</S.TextLink>
-    </a>
-    <a href="help/password">
-      <S.TextLink>비밀번호 찾기</S.TextLink>
-    </a>
-    <a href="register">
-      <S.TextLink>회원가입</S.TextLink>
-    </a>
-  </S.Footer>
-);
+export const LinkArea: React.FC = () => {
+  return (
+    <S.Footer>
+      <Link to="help/id">
+        <S.TextLink>아이디 찾기</S.TextLink>
+      </Link>
+      <Link to="help/password">
+        <S.TextLink>비밀번호 찾기</S.TextLink>
+      </Link>
+      <Link to="register">
+        <S.TextLink>회원가입</S.TextLink>
+      </Link>
+    </S.Footer>
+  );
+};
