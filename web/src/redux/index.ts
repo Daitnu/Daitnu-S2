75e215ac9b2a140dc48ed34c3f3f9e6b903d29e6
Seong <pok_gare@naver.com>
2020-05-13 19:07:28 +0900

    feat(fe/category): 루트 리듀서, 사가에 메일함 get 리듀서, 사가 추가

--- web/src/redux/index.ts
+++ web/src/redux/index.ts
@@ -3,13 +3,14 @@ import { all } from 'redux-saga/effects';
 import user from './global/user';
 import userRegister, { userRegisterSaga } from './user/register';
 import userLogin, { userLoginSaga } from './user/login';
+import categoryGet, { categoryGetSaga } from './category/get';
 
-const rootReducer = combineReducers({ user, userRegister, userLogin });
+const rootReducer = combineReducers({ user, userRegister, userLogin, categoryGet });
 
 export default rootReducer;
 
 export type RootState = ReturnType<typeof rootReducer>;
 
 export function* rootSaga() {
-  yield all([userRegisterSaga(), userLoginSaga()]);
+  yield all([userRegisterSaga(), userLoginSaga(), categoryGetSaga()]);
 }
