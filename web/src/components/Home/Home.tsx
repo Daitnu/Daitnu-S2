7cbe8d5c2978583606db56fc794d71f1aff5783a
Seong <pok_gare@naver.com>
2020-05-13 20:08:35 +0900

    feat(fe/home): useeffect로 category 요청하는 코드 추가

--- web/src/components/Home/Home.tsx
+++ web/src/components/Home/Home.tsx
@@ -1,15 +1,24 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import { RootState } from '~/redux';
+import React, { useEffect } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
 import { useHistory } from 'react-router-dom';
+import { RootState } from '~/redux';
+import { categoryGetRequest } from '~/redux/category/get';
 import * as S from './styled';
 
 export const Home: React.FC = () => {
   const user = useSelector((state: RootState) => state.user);
+  const { loading, data, error } = useSelector((state: RootState) => state.categoryGet);
+  const dispatch = useDispatch();
   const history = useHistory();
+
   if (user.id === '') {
     history.push('/login');
   }
+
+  useEffect(() => {
+    dispatch(categoryGetRequest());
+  }, []);
+
   return (
     <S.EntireWrapper>
       <S.TopWrapper>Top</S.TopWrapper>
