4a7285fbfa7b7981861f93298dc358aadb72c123
Seong <pok_gare@naver.com>
2020-05-13 16:55:11 +0900

    fix(fe/types): api state 파일에서 import 빼먹은것 import
    빨간줄이 안떠서 에러 없는줄..

--- web/src/@types/state/api.d.ts
+++ web/src/@types/state/api.d.ts
@@ -1,3 +1,6 @@
+import { HTTPResponse } from '../response/success';
+import { BusinessErrorResponse } from '../response/error';
+
 export interface ApiState<T> {
   loading: boolean;
   data: null | HTTPResponse<T>;
