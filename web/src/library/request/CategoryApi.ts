999fbb30cd0f625b6198e9ac447dca0b1cabec87
Seong <pok_gare@naver.com>
2020-05-13 15:57:21 +0900

    feat(fe/api): category rename, delete api 추가

--- web/src/library/request/CategoryApi.ts
+++ web/src/library/request/CategoryApi.ts
@@ -3,7 +3,11 @@ import URL from './url';
 import { HTTPResponse } from '~/@types/response/success';
 import { ResponseCategory } from '~/@types/response/category';
 import { BusinessErrorResponse } from '~/@types/response/error';
-import { AddCategoryParam } from '~/@types/request/category';
+import {
+  AddCategoryParam,
+  RenameCategoryParam,
+  DeleteCategoryParam,
+} from '~/@types/request/category';
 
 export default class CategoryApi extends Api {
   constructor() {
@@ -21,5 +25,19 @@ export default class CategoryApi extends Api {
     url = URL.CATEGORY,
   ): Promise<HTTPResponse<ResponseCategory> | BusinessErrorResponse> {
     return this.post<ResponseCategory, AddCategoryParam>({ url, data });
+  }
+
+  public async renameCategory(
+    data: RenameCategoryParam,
+    url = URL.CATEGORY,
+  ): Promise<HTTPResponse<ResponseCategory> | BusinessErrorResponse> {
+    return this.patch<ResponseCategory, RenameCategoryParam>({ url, data });
+  }
+
+  public async deleteCategory(
+    data: DeleteCategoryParam,
+    url = URL.CATEGORY,
+  ): Promise<HTTPResponse<ResponseCategory> | BusinessErrorResponse> {
+    return this.delete<ResponseCategory, DeleteCategoryParam>({ url, data });
   }
 }
