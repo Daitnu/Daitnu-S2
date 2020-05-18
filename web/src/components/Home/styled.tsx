47079516a6467419cdc544b21a9a7a48b64465e1
Seong <pok_gare@naver.com>
2020-05-18 14:33:22 +0900

    feat(fe/home): 로딩 gif 추가

--- web/src/components/Home/styled.tsx
+++ web/src/components/Home/styled.tsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import * as GS from '../GlobalStyle';
+import LoadingGif from '~/assets/gif/loading.gif';
 
 export const EntireWrapper = styled(GS.FlexCenter)`
   width: 100%;
@@ -29,4 +30,10 @@ export const Body = styled(GS.FlexCenter)`
   width: 80%;
   height: 100%;
   border: 1px solid black;
+`;
+
+export const Loading = styled(GS.BackgroundImgDiv)`
+  width: 50px;
+  height: 50px;
+  background-image: url(${LoadingGif});
 `;
