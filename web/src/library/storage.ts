98eb888afe60569d44819076eb5794c9e907ec5c
Seong <pok_gare@naver.com>
2020-05-18 17:27:23 +0900

    feat(fe/storage): user info 전체를 들고오게끔 코드 추가

--- web/src/library/storage.ts
+++ web/src/library/storage.ts
@@ -3,5 +3,6 @@ export const USER_NAME = 'userName' as const;
 
 const getId = () => sessionStorage.getItem(USER_ID);
 const getName = () => sessionStorage.getItem(USER_NAME);
+const getUserInfo = () => ({ [USER_ID]: getId(), [USER_NAME]: getName() });
 
-export default { getId, getName };
+export default { getId, getName, getUserInfo };
