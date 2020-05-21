dcd4a24d297e7bfe093be2c3bb654b63b355797c
Seong <pok_gare@naver.com>
2020-05-21 18:28:02 +0900

    fix(be/test/mail): addMail 메소드, dto 수정에 따라 서비스 테스트 수정

--- server/src/test/java/daitnu/daitnus2/mail/MailServiceTest.java
+++ server/src/test/java/daitnu/daitnus2/mail/MailServiceTest.java
@@ -43,41 +43,40 @@ public class MailServiceTest {
     // when
     User user = accountsService.register(registerDTO);
     MailCategory category = mailCategoryService.makeDir("category1", user.getId());
-    MailTemplate mailTemplate = mailTemplateService
-      .makeMailTemplate("kimgogo@daitnu2.com", "kimsoso@daitnu2.com", "mail title1", "mail subject1");
-    MailAttachment mailAttachment = mailAttachmentService
-      .makeMailAttachment(mailTemplate.getId(), "png", "image1.png", "https://www.naver.com", 10L);
-    Mail mail = mailService.makeMail(category.getId(), user.getId(), mailTemplate.getId());
-    mailTemplate.addMail(mail);
-    mailTemplate.addAttachment(mailAttachment);
+
+    MailDTO.AddMailDTO addMailDTO = new MailDTO.AddMailDTO();
+    addMailDTO.setMailCategoryName(category.getName());
+    addMailDTO.setSubject("mail subject1");
+    addMailDTO.setTitle("mail title1");
+    addMailDTO.setMailReceivers("kimsoso@daitnu2.com");
+    addMailDTO.setFrom("kimgogo@daitnu2.com");
+    Mail mail = mailService.addMail(user.getId(), addMailDTO);
 
 
     // then
     User foundUser = accountsService.findOne(user.getId());
     MailCategory foundCategory = mailCategoryService.findOne(category.getId());
-    MailAttachment foundAttachment = mailAttachmentService.findOne(mailAttachment.getId());
-    MailTemplate foundTemplate = mailTemplateService.findOne(mailTemplate.getId());
+    MailTemplate foundTemplate = mailTemplateService.findAll().get(0);
     Mail foundMail = mailService.findOne(mail.getId());
 
     // - User Assert
     assertEquals(user.getId(), foundUser.getId());
-    assertEquals(1, user.getMailCategories().size());
+    assertEquals(5, user.getMailCategories().size());
 
     // - Category Assert
     assertEquals(category.getId(), foundCategory.getId());
-    assertEquals(1, mailCategoryService.findAll(user.getId()).size());
+    assertEquals(5, mailCategoryService.findAll(user.getId()).size());
     assertEquals(foundUser.getId(), foundCategory.getUser().getId());
-    assertEquals(foundCategory.getId(), foundUser.getMailCategories().get(0).getId());
 
+    // TODO: 첨부파일 기능 완료한 후 테스트 예정
     // - Mail Attachment
-    assertEquals(mailAttachment.getId(), foundAttachment.getId());
-    assertEquals(foundAttachment.getId(), foundTemplate.getMailAttachments().get(0).getId());
+//    assertEquals(mailAttachment.getId(), foundAttachment.getId());
+//    assertEquals(foundAttachment.getId(), foundTemplate.getMailAttachments().get(0).getId());
 
     // - Mail Template
-    assertEquals(mailTemplate.getId(), foundTemplate.getId());
     assertEquals(1, mailTemplateService.findAll().size());
-    assertEquals(1, foundTemplate.getMailAttachments().size());
-    assertEquals("kimsoso@daitnu2.com", mailTemplate.getMailReceivers());
+    assertEquals(0, foundTemplate.getMailAttachments().size());
+    assertEquals("kimsoso@daitnu2.com", foundTemplate.getMailReceivers());
 
     // - Mail
     assertEquals(mail.getId(), foundMail.getId());
